b68562a8e29dca72b39a4efd50c021d6eb76d7a8
McbeEringi <git@mcbeeringi.dev>
2025-08-20 15:08:53 +0900

    -

---
+++ cmd.mjs
@@ -0,0 +1,56 @@
+const
+cmd={
+	status:async({
+		log,
+		dld,
+		cfg,
+		ls
+	})=>log(JSON.stringify({
+		cfg,
+		dl:await ls(dld)
+	},0,'\t')+'\n'),
+	abort:({
+		log,
+		abort
+	})=>(abort.dispatchEvent(new CustomEvent('abort')),log(`Abort requested.\n`),0),
+	dl:({
+		log,
+		dld,
+		abort:signal,
+		x,
+		progress=(w,f,s)=>((r=w.body.getReader(),p=[0,+w.headers.get('content-length')],b,c=_=>(b=1,r.cancel()))=>Array.fromAsync({[Symbol.asyncIterator]:_=>(
+			s.addEventListener('abort',c,{once:1}),f(p),{next:async x=>(
+				x=(await r.read()).value,b&&await Promise.reject({message:'Load aborted.'}),x?(p[0]+=x.length,f(p)):s.removeEventListener('abort',c),{done:!x,value:x}
+			)}
+		)}))()
+	})=>(async()=>(
+		log('Checking update...\n'),
+		x=new URL(
+			(await(await fetch('https://net-secondary.web.minecraft-services.net/api/v1.0/download/links',{signal})).json())?.result?.links?.find?.(
+				(s=>x=>x.downloadType==s)({win32:'serverBedrockWindows',linux:'serverBedrockLinux'}[process.platform])
+			)?.downloadUrl||await Promise.resolve(new Error(`Can not find URL for platform "${process.platform}".`))
+		),
+		x.name=x.pathname.match(/[^/]+$/)?.[0],
+		x.file=Bun.file(`${dld}/${x.name}`),
+		log(`latest: ${x.name}`),
+		await x.file.exists()?(
+			log(`Already up to date!\ndl: Done.\n`)
+		):(
+			log(`New version!\nDownloading...\n`),
+			await Bun.write(x.file.name,new Blob(await progress(
+				await fetch(x,{signal}),
+				([x,a])=>log(`${(x/a*100).toFixed(2).padStart(6)} %`),
+				signal
+			)),log(`100.00 %\n`)),
+			log(`dl: Done.\n`)
+		),
+		0
+	))().catch(e=>(log(`${e.message}\ndl: Aborted.\n`),e)),
+	deploy:_=>_,
+	start:_=>_,
+	pkill:_=>_,
+	help:({
+		log
+	})=>log(`known commands:\n${Object.keys(cmd).map(x=>`\t${x}\n`).join('')}`)
+};
+export{cmd};
---
+++ main.mjs
@@ -0,0 +1,69 @@
+#!/bin/env -S bun
+
+import{cmd}from'./cmd.mjs';
+// import{}from'node:fs/promises';
+Bun.$.throws(0);
+
+const
+svr_id=Bun.argv[2],
+dld=`downloads`,
+svrsd=`servers`,
+svrd=`${svrsd}/${svr_id}`,
+bind=`${svrd}/bin`,
+libd=`${svrd}/lib`;
+
+svr_id||await Promise.reject('Server ID not specified!');
+
+const
+port_pool=[...Array(10)].map((_,i)=>({
+	http:3000+i,
+	mcbe_ipv4:19200+i*2,
+	mcbe_ipv6:19201+i*2
+})),
+ls=async x=>(await Bun.$`ls ${x}`.text()).match(/.+?(?=\n)/g)??[],
+cfg=await(async(n,w=Bun.file(`${svrd}/${n}`))=>(
+	await w.exists()?await w.json():await(async x=>(
+		x=[...await(await ls(svrsd)).reduce(async(a,x)=>(
+			x=Bun.file(`${svrsd}/${x}/${n}`),
+			await x.exists()&&(await a).delete((await x.json())?.port_pool_index),
+			a
+		),new Set(Object.keys(port_pool).map(x=>+x)))],
+		await Bun.write(w.name,JSON.stringify(x={
+			port_pool_index:x[0],
+			lib:[
+				'worlds',
+				'allowlist.json',
+				'permissions.json',
+				'server.properties'
+			],
+			auth:{
+				admin:'admin'
+			},
+			webhook:[]
+		},0,'\t')),x
+	))()
+))('config.json'),
+
+abort=new EventTarget(),
+
+
+auth=({cookies:c,headers:h})=>((w,a='Authorization')=>(
+	w.includes(c.get(a))?(c.set({name:a,value:c.get(a),maxAge:3600}),null):
+	w.includes(h.get(a))?(c.set(a,h.get(a)),new Response(null,{headers:{Refresh:0}})):
+	new Response(null,{status:401,headers:{'WWW-Authenticate':'Basic realm="main"'}})
+))(Object.entries(cfg.auth).map(([u,p])=>`Basic ${btoa(`${u}:${p}`)}`)),
+http=Bun.serve({
+	port:port_pool[cfg.port_pool_index].http,
+	routes:{
+		'/':r=>auth(r)||new Response(Bun.file('./assets/index.html')),
+		'/ws':(r,s)=>auth(r)||(s.upgrade(r),new Response()),
+		'/favicon.ico':(r,s)=>new Response(Bun.file('./assets/favicon.ico'))
+	},
+	websocket:{
+		open:x=>console.log('open'),
+		message:(x,msg)=>(cmd[msg]??(({log})=>log(`Unknown command "${msg}"\n`)))({log:x=>Bun.stdout.write(`\x1b[2K\x1b[0G${x}`),dld,abort,cfg,ls}),
+		close:x=>console.log('closed')
+	}
+});
+
+console.log(port_pool[cfg.port_pool_index]);
