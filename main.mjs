89d5e6772d4c152e550188b478154ce8ca1c1878
McbeEringi <git@mcbeeringi.dev>
2025-09-13 01:55:11 +0900

    -

--- main.mjs
+++ main.mjs
@@ -2,22 +2,24 @@
 
 import{BDSMGR}from'./bdsmgr.mjs';
 
-const w=await BDSMGR.init({server_id:void 0});
-await w.deploy();
+// const w=await BDSMGR.init({server_id:void 0});
+// await w.deploy();
 // console.log(w);
 
-//
-// const
-// http=Bun.serve({
-// 	port:3000,
-// 	routes:{
-// 		'/a/ws':(r,s)=>(console.log(r),s.upgrade(r,{data:new URL(r.url).pathname}),new Response()),
-// 		'/b/ws':(r,s)=>(s.upgrade(r),new Response()),
-// 		'/favicon.ico':(r,s)=>new Response(Bun.file('./assets/favicon.ico'))
-// 	},
-// 	fetch(r,s){console.log(r,s);return new Response();},
-// 	websocket:{
-// 		open:x=>console.log(x),
-// 		message:(x,msg)=>console.log(x,msg)
-// 	}
-// });
+
+const
+http=Bun.serve({
+	port:3000,
+	routes:{
+		'/':(r,s)=>new Response('hello!'),
+		'/:id':(r,s)=>Response.redirect(`/${r.params.id}/`),
+		'/:id/':(r,s)=>new Response(r.params.id),
+		'/:id/ws':(r,s)=>(s.upgrade(r,{data:r.params.id}),new Response()),
+		'/favicon.ico':(r,s)=>new Response(Bun.file('./assets/favicon.ico'))
+	},
+	// fetch(r,s){console.log(r,s);return new Response();},
+	websocket:{
+		open:x=>console.log(x),
+		message:(x,msg)=>console.log(x,msg)
+	}
+});
